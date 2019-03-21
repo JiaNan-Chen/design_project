25eb80895696d112b69a8c2f94aa760519291f84
chenjianan <chenjianan@huya.com>
2019-03-21 15:19:56 +0800

    [FEATURE]添加项目

---
+++ android/app/src/main/java/design/example/com/designpro/view/AbstractRender.java
@@ -0,0 +1,10 @@
+package design.example.com.designpro.view;
+
+import android.graphics.Canvas;
+
+/**
+ * @author HUYA JiaNan
+ */
+public interface AbstractRender {
+    void draw(Canvas canvas);
+}
---
+++ android/app/src/main/java/design/example/com/designpro/view/FloatingBuilder.java
@@ -0,0 +1,84 @@
+package design.example.com.designpro.view;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import design.example.com.designpro.R;
+
+/**
+ * @author HUYA JiaNan
+ */
+public abstract class FloatingBuilder<T extends View> {
+    Context mContext;
+
+    public T getContentView() {
+        return mContentView;
+    }
+
+    T mContentView;
+
+    public FloatingBuilder(Context context) {
+        this.mContext = context;
+    }
+
+    public abstract T createView();
+
+    public abstract WindowManager.LayoutParams createWindowManagerParams(int left, int top) throws Exception;
+
+    public static class RenderFloating extends FloatingBuilder<RenderView> {
+
+        private static RenderFloating singleton;
+
+        public static RenderFloating getInstance(Context context) {
+            if (singleton == null) {
+                synchronized (RenderFloating.class) {
+                    if (singleton == null) {
+                        singleton = new RenderFloating(context);
+                    }
+                }
+            }
+            return singleton;
+        }
+
+        public RenderFloating(Context context) {
+            super(context);
+        }
+
+        @Override
+        public RenderView createView() {
+            if (mContentView == null) {
+                mContentView = (RenderView) LayoutInflater.from(mContext).inflate(R.layout.render_layout, null);
+            }
+            return mContentView;
+        }
+
+        @Override
+        public WindowManager.LayoutParams createWindowManagerParams(int left, int top) throws Exception {
+            if (mContentView == null) {
+                throw new Exception("get null content view");
+            }
+            Log.i(getClass().getName(), "width:" + mContentView.getWidth()
+                    + "   height:" + mContentView.getMeasuredWidth());
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+            params.gravity = Gravity.TOP | Gravity.LEFT;
+            params.x = left;
+            params.y = top;
+            //8.0新特性
+            if (Build.VERSION.SDK_INT >= 26) {
+                params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+            } else {
+                params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+            }
+            params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+            params.format = PixelFormat.TRANSLUCENT;
+            return params;
+        }
+    }
+}
